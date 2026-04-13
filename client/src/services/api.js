08f04730afdf0af00a0c4d763bdb7e3b06a93d80
Mourad El Jayi <m.eljayi@ormvasm.local>
2026-04-13 11:03:10 +0100

    Split frontend and json-server backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3001";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getProducts = () =>
     fetch(`${API_URL}/products`).then(res => res.json());
